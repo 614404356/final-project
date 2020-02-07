8f9591516e4487a280b2380d9069a6ce6032780d
614404356 <zhangqi8321@qq.com>
2020-02-07 19:30:17 +0800

    test1

--- src/main/java/com/dasi/graduation/finalproject/controller/test.java
+++ src/main/java/com/dasi/graduation/finalproject/controller/test.java
@@ -5,6 +5,6 @@ import org.apache.logging.log4j.util.Strings;
 public class test {
 
     public static void main(Strings[] args){
-        System.out.println("it is test");
+        System.out.println("it is test11");
     }
 }
