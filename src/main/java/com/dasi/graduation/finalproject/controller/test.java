77ecd08078c4e2f98adc2ba7d74441f6ecc99c2a
614404356 <zhangqi8321@qq.com>
2020-02-07 19:04:14 +0800

    test System

--- src/main/java/com/dasi/graduation/finalproject/controller/test.java
+++ src/main/java/com/dasi/graduation/finalproject/controller/test.java
@@ -1,4 +1,10 @@
 package com.dasi.graduation.finalproject.controller;
 
+import org.apache.logging.log4j.util.Strings;
+
 public class test {
+
+    public static void main(Strings[] args){
+        System.out.println("it is test");
+    }
 }
